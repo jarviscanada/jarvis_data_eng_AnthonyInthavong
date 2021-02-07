4e6c96d3d4f7fb8826eafd9d91c185b85e8a2bb3
anth7310 <anthonyin98@hotmail.ca>
2021-02-07 22:17:40 +0000

    added 5 minute interval for memory consumption in percentage

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -8,5 +8,29 @@ SELECT cpu_number, host_id, total_mem FROM
         rank() OVER (PARTITION BY cpu_number ORDER BY total_mem DESC)
     FROM
         "public".host_info
-) AS ss ORDER BY cpu_number ASC, total_mem DESC
-;
+) AS ss ORDER BY cpu_number ASC, total_mem DESC;
+
+-- average used memory percentage in 5 minute intervals
+SELECT DISTINCT ON ("timestamp") host_id, host_name, "timestamp", avg_used_mem_percentage
+FROM (
+    SELECT
+        host_id,
+        host_name,
+        "timestamp",
+        AVG(FLOOR((total_mem - memory_free)*100/total_mem))
+            OVER (PARTITION BY "timestamp")
+            AS avg_used_mem_percentage
+    FROM (
+        SELECT
+            host_id,
+            hostname AS host_name,
+            to_timestamp((extract("epoch" FROM "public".host_usage."timestamp")::int / 300) * 300) as "timestamp",
+            total_mem,
+            memory_free
+        FROM
+            "public".host_usage JOIN "public".host_info
+        ON
+            "public".host_usage.host_id = "public".host_info.id
+    ) AS ss
+
+ORDER BY "timestamp" ASC) tt;
