30d43a9a83de7572ee7efd3afb415fd46c0e38a9
anth7310 <anthonyin98@hotmail.ca>
2021-02-11 00:43:12 -0500

    implemented regex

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/practice/RegexExc.java
@@ -0,0 +1,28 @@
+package ca.jrvs.apps.grep.practice;
+
+public interface RegexExc {
+    /**
+     * Return true if filename extension is jpg or jpeg (case insensitive)
+     *
+     * @param filename
+     * @return
+     */
+    public boolean matchJpeg(String filename);
+
+    /**
+     * return true if ip is valid
+     * to simplify the problem, IP address range is from 0.0.0.0 to 999.999.999.999
+     *
+     * @param ip
+     * @return
+     */
+    public boolean matchIp(String ip);
+
+    /**
+     * return true if line is empty (e.g. empty, white space, tabs, etc...)
+     * @param line
+     * @return
+     */
+    public boolean isEmptyLine(String line);
+}
+
