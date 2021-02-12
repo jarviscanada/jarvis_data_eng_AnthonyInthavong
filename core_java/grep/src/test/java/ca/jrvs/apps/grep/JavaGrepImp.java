5741d417c1ceb765dc80d4f2ff3304a73c8484d6
anth7310 <anthonyin98@hotmail.ca>
2021-02-12 17:11:26 -0500

    base code for implementation of javagrep class

---
+++ core_java/grep/src/test/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,93 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JavaGrepImp implements JavaGrep {
+
+  final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
+
+  private String regex;
+  private String rootPath;
+  private String outFile;
+
+  public static void main(String[] args) {
+    if (args.length != 3) {
+      throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
+    }
+
+    //Use default logger config
+    BasicConfigurator.configure();
+
+    JavaGrepImp javaGrepImp = new JavaGrepImp();
+    javaGrepImp.setRegex(args[0]);
+    javaGrepImp.setRootPath(args[1]);
+    javaGrepImp.setOutFile(args[2]);
+
+    try {
+      javaGrepImp.process();
+    } catch (Exception ex) {
+      javaGrepImp.logger.error(ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public void process() throws IOException {
+
+  }
+
+  @Override
+  public List<File> listFiles(String rootDir) {
+    return null;
+  }
+
+  @Override
+  public List<String> readLines(File inputFile) {
+    return null;
+  }
+
+  @Override
+  public boolean containsPattern(String line) {
+
+    return false;
+  }
+
+  @Override
+  public void writeToFile(List<String> lines) throws IOException {
+
+  }
+
+  @Override
+  public String getRootPath() {
+    return this.rootPath;
+  }
+
+  @Override
+  public void setRootPath(String rootPath) {
+    this.rootPath = rootPath;
+  }
+
+  @Override
+  public String getRegex() {
+    return this.regex;
+  }
+
+  @Override
+  public void setRegex(String regex) {
+    this.regex = regex;
+  }
+
+  @Override
+  public String getOutFile() {
+    return this.outFile;
+  }
+
+  @Override
+  public void setOutFile(String outFile) {
+    this.outFile = outFile;
+  }
+}
