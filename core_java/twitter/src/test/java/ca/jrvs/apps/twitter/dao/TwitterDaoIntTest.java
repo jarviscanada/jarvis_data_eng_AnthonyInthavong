dbc9ea3470960c95443b6a5360ad06f879d5c7ec
anth7310 <anthonyin98@hotmail.ca>
2021-03-23 02:14:17 -0400

    Integration and Unit test

---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
@@ -0,0 +1,93 @@
+package ca.jrvs.apps.twitter.dao;
+
+import static org.junit.Assert.*;
+
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
+import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
+import ca.jrvs.apps.twitter.model.Tweet;
+import ca.jrvs.apps.twitter.util.TweetUtil;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.mockito.Mockito.verify;
+
+public class TwitterDaoIntTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(TwitterDaoIntTest.class);
+
+  HttpHelper httpHelper;
+  CrdDao dao;
+  Tweet tweet;
+  String text;
+  Double longitude;
+  Double latitude;
+  String hashTag;
+
+  @Before
+  public void setUp() throws UnsupportedEncodingException {
+    BasicConfigurator.configure();
+    String consumerKey = System.getenv("consumerKey");
+    String consumerSecret = System.getenv("consumerSecret");
+    String accessToken = System.getenv("accessToken");
+    String tokenSecret = System.getenv("tokenSecret");
+    httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
+    dao = new TwitterDao(httpHelper);
+
+    // minimum requirements to build tweet object
+    hashTag = "#abc";
+    text = "sometext " + hashTag + " " + System.currentTimeMillis();
+    longitude = -1d;
+    latitude = 1d;
+
+    tweet = TweetUtil.buildTweet(
+        URLEncoder.encode(text, "UTF-8"),
+        longitude, latitude
+    );
+  }
+
+
+
+  @Test
+  public void create() {
+    tweet = (Tweet) dao.create(tweet);
+    assertNotNull(tweet);
+    assertEquals(text, tweet.getText());
+//    assertEquals(longitude, tweet.getCoordinates().getCoordinates().get(0));
+//    assertEquals(latitude, tweet.getCoordinates().getCoordinates().get(1));
+
+    // tear down - delete tweet
+    try {
+      logger.error(tweet.getText());
+      dao.deleteById(tweet.getIdStr());
+    } catch (Exception e) {
+      logger.error("Tweet ID not found: {}", tweet.getIdStr());
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void findById() {
+    tweet = (Tweet) dao.create(tweet);
+    tweet = (Tweet) dao.findById(tweet.getIdStr());
+
+    assertEquals(text, tweet.getText());
+//    assertEquals(longitude, tweet.getCoordinates().getCoordinates().get(0));
+//    assertEquals(latitude, tweet.getCoordinates().getCoordinates().get(1));
+  }
+
+  @Test
+  public void deleteById() {
+    tweet = (Tweet) dao.create(tweet);
+    tweet = (Tweet) dao.deleteById(tweet.getIdStr());
+    assertEquals(text, tweet.getText());
+//    assertEquals(longitude, deletedTweet.getCoordinates().getCoordinates().get(0));
+//    assertEquals(latitude, deletedTweet.getCoordinates().getCoordinates().get(1));
+  }
+}
---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoUnitTest.java
@@ -0,0 +1,144 @@
+package ca.jrvs.apps.twitter.dao;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
+import ca.jrvs.apps.twitter.model.Tweet;
+import ca.jrvs.apps.twitter.util.JsonUtil;
+import ca.jrvs.apps.twitter.util.TweetUtil;
+import java.io.IOException;
+import java.net.URLEncoder;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TwitterDaoUnitTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(TwitterDaoUnitTest.class);
+
+  @Mock
+  HttpHelper mockHelper;
+
+  @InjectMocks
+  TwitterDao dao;
+
+  String tweetJsonStr;
+
+  Tweet expectedTweet;
+
+  @Before
+  public void setUp() throws Exception{
+    BasicConfigurator.configure();
+    tweetJsonStr = "{\n" +
+        "  \"created_at\" : \"Thu Jun 04 01:46:18 +0000 2020\",\n" +
+        "  \"id\" : 1268358385075961862,\n" +
+        "  \"id_str\" : \"1268358385075961862\",\n" +
+        "  \"text\" : \"timestamp20159115163212\",\n" +
+        "  \"entities\" : {\n" +
+        "    \"hashtags\" : [ ],\n" +
+        "    \"user_mentions\" : [ ]\n" +
+        "  },\n" +
+        "  \"coordinates\" : {\n" +
+        "    \"coordinates\" : [ 10.0, 1.0 ],\n" +
+        "    \"type\" : \"Point\"\n" +
+        "  },\n" +
+        "  \"retweet_count\" : 0,\n" +
+        "  \"retweeted\" : false,\n" +
+        "  \"entites\" : {\n" +
+        "    \"hashtags\" : [ ],\n" +
+        "    \"user_mentions\" : [ ]\n" +
+        "  },\n" +
+        "  \"favourite_count\" : 0\n" +
+        "}";
+
+    expectedTweet = JsonUtil.toObjectFromJson(tweetJsonStr, Tweet.class);
+  }
+
+  @Test
+  public void create() throws IOException {
+    //test failed request
+    String hashTag = "#abc";
+    String text = "sometext " + hashTag + " " + System.currentTimeMillis();
+    text = URLEncoder.encode(text, "UTF-8");
+    Double lat = 1d;
+    Double lon = -1d;
+    //expectation is expected here
+    when(mockHelper.httpPost(isNotNull())).thenThrow(new RuntimeException("mock"));
+    try {
+      dao.create(TweetUtil.buildTweet(text, lon, lat));
+      fail();
+    } catch (RuntimeException e) {
+      assertTrue(true);
+    }
+    verify(mockHelper).httpPost(isNotNull());
+
+    //Test happy path
+    //however, we don't want to call parseResponseBody.
+    //we will make a spyDao which can fake parseResponseBody return value
+    when(mockHelper.httpPost(isNotNull())).thenReturn(null);
+    TwitterDao spyDao = spy(dao);
+    //mock parseResponseBody
+    doReturn(expectedTweet).when(spyDao).parseResponseBody(any(), anyInt());
+
+
+    Tweet tweet = spyDao.create(TweetUtil.buildTweet(text, lon, lat));
+    verify(mockHelper, times(2)).httpPost(isNotNull());
+    assertNotNull(tweet);
+    assertNotNull(tweet.getText());
+  }
+
+  @Test
+  public void findById(){
+
+    // test failed request
+    when(mockHelper.httpGet(isNotNull())).thenThrow(new RuntimeException("mock"));
+    try {
+      dao.findById("");
+      fail();
+    } catch (RuntimeException e){
+      assertTrue(true);
+    }
+
+    // verify httpGet is called
+    verify(mockHelper).httpGet(isNotNull());
+
+    // test successful request
+    TwitterDao spyDao = spy(dao);
+
+    when(mockHelper.httpGet(isNotNull())).thenReturn(null);
+    // mock parseResponseBody
+    doReturn(expectedTweet).when(spyDao).parseResponseBody(any(),anyInt());
+    Tweet tweet = spyDao.findById("1268358385075961862");
+    verify(mockHelper, times(2)).httpGet(isNotNull());
+    assertNotNull(tweet);
+    assertNotNull(tweet.getText());
+
+  }
+
+  @Test
+  public void deleteById() throws Exception {
+    TwitterDao spyDao = spy(dao);
+    doReturn(expectedTweet).when(spyDao).parseResponseBody(any(), anyInt());
+    Tweet tweet = spyDao.deleteById("1268358385075961862");
+    verify(mockHelper).httpPost(isNotNull());
+    assertNotNull(tweet);
+    assertNotNull(tweet.getText());
+
+  }
+}
